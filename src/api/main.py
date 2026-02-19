647ab01033c03be93618dffc974081fc14a1b1cf
mohanreddy <mohanreddykambham1515@gmail.com>
2026-02-19 21:23:11 +0530

    fix predict endpoint

--- src/api/main.py
+++ src/api/main.py
@@ -3,10 +3,19 @@ import joblib
 
 app = FastAPI()
 
-# load model
 model = joblib.load("models/fraud_model.pkl")
 
 
 @app.get("/")
 def home():
     return {"message": "API is running"}
+
+
+@app.get("/predict")
+def predict(amount: float, avg_last_5_amount: float, num_prev_transactions: int):
+
+    sample = [[amount, avg_last_5_amount, num_prev_transactions]]
+
+    prediction = model.predict(sample)[0]
+
+    return {"prediction": int(prediction)}
